2a0038326b491b7013c45e503ffb06ef52668581
binyu_J <yubinlove1991@gmail.com>
2015-07-03 05:59:35 +0800

    修正匹配到其他url，加上对playlist的支持

--- src/background.js
+++ src/background.js
@@ -1,15 +1,19 @@
 chrome.webRequest.onBeforeRequest.addListener(
   // callack
-  function(details) {
+  function (details) {
     var requestUrl = details.url;
-    if (requestUrl.indexOf("video/h5") == -1){   // url中不包含h5时indexOf返回-1
-      var newUrl = requestUrl.replace(/video/, "video/h5");
-      return {redirectUrl: newUrl};
+    var newUrl;
+    if (requestUrl.search(/video\/\d+/) !== -1) {
+      newUrl = requestUrl.replace(/video/, "video/h5");
+    } else if (requestUrl.search(/playlist\/\d+/) !== -1) {
+      newUrl = requestUrl.replace(/playlist/, "playlist/h5");
     }
+    return {redirectUrl: newUrl};
   },
   // filters
   {
     urls: ["http://v.yinyuetai.com/*"],
   },
   // extraInfoSpec
-  ["blocking"]);
+  ["blocking"]
+);
